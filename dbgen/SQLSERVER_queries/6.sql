a5e20cce8b71afebb1b189fa57887d289fade1f5
SpyrosChouliaras <spyroshouliaras@gmail.com>
2020-11-16 16:52:29 +0000

    SQLSERVER queries

---
+++ dbgen/SQLSERVER_queries/6.sql
@@ -0,0 +1,14 @@
+-- $ID$
+-- TPC-H/TPC-R Forecasting Revenue Change Query (Q6)
+-- Functional Query Definition
+-- Approved February 1998
+ect
+	sum(l_extendedprice * l_discount) as revenue
+from
+	LINEITEM
+where
+	l_shipdate >= date '1994-01-01'
+	and l_shipdate < date '1994-01-01' + interval '1994-01-01' year
+	and l_discount between : 0.06 - 0.01 and 0.06 + 0.01
+	and l_quantity < 24;
+
