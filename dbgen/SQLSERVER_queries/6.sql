a49091a5f3a110a7892d28f85c2d553e56564689
SpyrosChouliaras <spyroshouliaras@gmail.com>
2020-11-18 16:35:40 +0000

    Update 6.sql

--- dbgen/SQLSERVER_queries/6.sql
+++ dbgen/SQLSERVER_queries/6.sql
@@ -9,6 +9,6 @@ from
 where
 	l_shipdate >= date '1994-01-01'
 	and l_shipdate < date '1994-01-01' + interval '1994-01-01' year
-	and l_discount between : 0.06 - 0.01 and 0.06 + 0.01
+	and l_discount between 0.06 - 0.01 and 0.06 + 0.01
 	and l_quantity < 24;
 
